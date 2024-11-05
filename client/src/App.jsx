07ba130eca3c1897f2c4c653d1e4e908df32ed45
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-05 08:47:32 +0530

    Admin Login added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+
 let user = true;
 
 const App = () => {
@@ -30,6 +32,7 @@ const App = () => {
               </ProtectRoute>
             }
           />
+          <Route path="/admin" element={<AdminLogin />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
