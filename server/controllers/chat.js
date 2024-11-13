2208ad14ddec01c7322a694d8e0785e0edc89db3
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-11-13 21:03:15 +0530

    changes updated in chat controller

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -3,6 +3,7 @@ import { ErrorHandler } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
 import { emitEvent } from "../utils/features.js";
 import { ALERT, REFETCH_CHATS } from "../constants/events.js";
+import { getOtherMember } from "../lib/helper.js";
 
 const newGroupChat = TryCatch(async (req, res, next) => {
   const { name, members } = req.body;
@@ -29,4 +30,44 @@ const newGroupChat = TryCatch(async (req, res, next) => {
   });
 });
 
-export { newGroupChat };
+const getMyChats = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({ members: req.user }).populate(
+    "members",
+    "name username avatar"
+  );
+
+  const transformedChats = chats.map(
+    ({ _id, name, groupChat, members, lastMessage }) => {
+      const otherMember = getOtherMember(members, req.user);
+      return {
+        _id,
+        groupChat,
+        avatar: groupChat
+          ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+          : [otherMember.avatar.url],
+        name: groupChat ? name : otherMember.name,
+        members: members.reduce((prev, curr) => {
+          if (curr._id.toString() !== req.user.toString()) {
+            prev.push(curr._id);
+          }
+          return prev;
+        }, []),
+        // lastMessage: lastMessage,
+      };
+    }
+  );
+
+  return res.status(200).json({
+    success: true,
+    chats: transformedChats,
+  });
+});
+
+const getMyGroups = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.user,
+    groupChat: true,
+  }).populate("members", "name avatar");
+});
+
+export { newGroupChat, getMyChats, getMyGroups };
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newGroupChat } from "../controllers/chat.js";
+import { getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
 
 const app = express.Router();
 
 // After here user must be logged in to access the routes
 app.use(isAuthenticated);
 app.post("/new", newGroupChat);
+app.get("/my", getMyChats);
+app.get("/-----", getMyGroups);
 
 export default app;
